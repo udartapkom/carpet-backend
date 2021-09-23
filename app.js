c569da6151ecc8b933898e51b48498f20ac24f0c
udartapkom <karakoom@ya.ru>
2021-09-23 15:02:44 +0300

    Добавлен функционал_1

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routers = require('./routers/index');
+const cors = require('cors');
 
 const CFG = require('./utils/config'); //конфиг
 const { PORT = CFG.PORT, MONGO_URL = CFG.MONGO_URL } = process.env; //получаем константы для подключения mongoDB
+
+const allowedCors = [
+  'http://www.myportfolios.ru/',
+  'http://myportfolios.ru/',
+  'https://www.myportfolios.ru/',
+  'https://myportfolios.ru/',
+  '*',
+];
+
 const app = express();
 
 mongoose.connect(MONGO_URL,  //коннектимся к БД
@@ -15,6 +25,14 @@ mongoose.connect(MONGO_URL,  //коннектимся к БД
     });
   
   mongoose.connection.on('open', () => console.log('connected to mongoDB')); //выводим в консоль сообщение, если коннект успешен 
+  app.use((req, res, next) => {
+    const { origin } = req.headers;
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    next();
+  });
+  app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', routers);
